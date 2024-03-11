9433d99c1b4f2e852b505ee50c0566afa5458e88
SebileGrgl <livaliva998@gmail.com>
2024-03-12 00:06:46 +0300

    PR-1 Search inputa ürün arama özelliği eklendi

---
+++ app.js
@@ -0,0 +1,58 @@
+//Sebile/Beginning of Homepage-Header
+// Language start
+const languages = [
+  {
+    id: "en",
+    name: "English",
+  },
+  {
+    id: "tr",
+    name: "Turkish",
+  },
+];
+const selectBtn = document.querySelector("#select-btn");
+const option1 = document.querySelector("#value1");
+option1.textContent = languages[0].name;
+
+selectBtn.addEventListener("click", () => {
+  selectBtn.innerHTML = languages.map(
+    (lang) => `<option>${lang.name}</option>`
+  );
+});
+// Language end
+
+//Searchbar start
+const searchIcon = document.querySelector("#header-search-icon");
+const searchInput = document.querySelector(".searchbar input");
+const listbar = document.querySelector("#product-list");
+
+searchInput.addEventListener("input", async function () {
+  const inputValue = searchInput.value.trim().toLowerCase();
+
+  const response = await fetch("https://fakestoreapi.com/products");
+  const dataProducts = await response.json();
+
+  const filteredProducts = dataProducts.filter((product) =>
+    product.title.toLowerCase().includes(inputValue)
+  );
+  listbar.classList.add("product-lists");
+  if (filteredProducts.length === 0) {
+    listbar.innerHTML = "<p>Ürün Bulunamadı</p>";
+  } else {
+    listbar.innerHTML = filteredProducts
+      .map((item) => `<li><a href="#">${item.title}</a></li>`)
+      .join("");
+  }
+
+  if (inputValue === "") {
+    listbar.innerHTML = "";
+    listbar.classList.remove("product-lists");
+  }
+});
+
+//Searchbar end
+
+//Slider start
+
+//Slider end
+//Sebile/End of Homepage-Header
