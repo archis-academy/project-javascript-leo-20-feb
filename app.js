c0877e31ab6ed682f89aa741d5846ff25e2ef14a
SebileGrgl <livaliva998@gmail.com>
2024-03-13 22:20:56 +0300

    PR-1 Carousel resimler eklendi.Responsive tasarım yapıldı.

--- app.js
+++ app.js
@@ -105,38 +105,33 @@ womanLink.addEventListener("mouseleave", () => {
 });
 //Woman-Men Pop-up end
 
-//Slider start
-let slideIndex = 3;
-showSlides(slideIndex);
+//Carousel start
+const carouselImage = document.querySelector("#carouselImage");
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
+const carouselDots = document.querySelectorAll(".dot");
+
+function handleDotClick(index) {
+  carouselDots.forEach((dot, i) => {
+    dot.classList.toggle("active-dot", i === index);
+  });
+  carouselImage.src = `./images/carousel${index + 1}.png`;
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+carouselDots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    handleDotClick(index);
+  });
+});
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("my-slides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active-dot", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active-dot";
-}
-//Slider end
+const menuBtn = document.querySelector("#menuBtn");
+const headerNavbar = document.querySelector(".header-navbar");
+menuBtn.addEventListener("click", () => {
+  headerNavbar.classList.toggle("show-navbar");
+});
+
+headerNavbar.addEventListener("click", () => {
+  headerNavbar.classList.toggle("show-navbar");
+});
+//Carousel end
+
 //Sebile/End of Homepage-Header
