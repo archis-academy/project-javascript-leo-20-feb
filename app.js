7e7ba9f238728d741b88c8401fbc503a1c2099c3
yusraylvz <128381663+yusraylvz@users.noreply.github.com>
2024-03-20 06:25:08 +0300

    [PR-2] setHeartIcons() ve setCartIcons() eklendi.

--- app.js
+++ app.js
@@ -13,54 +13,62 @@ async function getProducts() {
     console.error("Hata oluştu.", error);
   }
 }
+getProducts();
 
 function showProducts() {
   const productsToShow = products.slice(currentIndex, currentIndex + 4);
   productCarousel.innerHTML = productsToShow
     .map((product) => {
       return `<div class="product-card">
-                    <img class ="product-card-img" src ="${
-                      product.image
-                    }" alt = "${product.title}" />
-                    <button onclick="addToCart(${
-                      product.id
-                    })" class="add-to-cart-btn" >Add To Cart</button>
+                    <img class ="product-card-img" src ="${product.image
+        }" alt = "${product.title}" />
+                    <button onclick="addToCart(${product.id
+        })" class="add-to-cart-btn" >Add To Cart</button>
                     <p class = "discount-rate">-50%</p>
                     <h3 class ="product-title">${product.title}</h3>
                     <div class="product-prices-container">
-                        <p class ="product-price-discounted">$${
-                          product.price * 0.5
-                        }</p>
+                        <p class ="product-price-discounted">$${(
+          product.price * 0.5
+        ).toFixed(2)}</p>
                         <s class ="product-price"> $${product.price}</s>
                      </div>
-                    <p>${getStars(product.rating.rate)} (${product.rating.count})</p>
-                     
+                    <p>${getStars(product.rating.rate)} (${product.rating.count
+        })</p>
+
     
                     <div class="product-card-icons">
-                        <img onclick="addToWishlist(${
-                          product.id
-                        })" src="images/wishlist-icon.svg" class="wishlist-icon" />
-                        <img onclick="addToCart(${
-                          product.id
-                        })" src="images/cart-icon.svg"  class="cart-icon" />
+                        <svg onclick="addToWishlist(${product.id
+        })" width="28" height="28" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path id="heartIcon${product.id
+        }" d="M11 7C8.239 7 6 9.216 6 11.95C6 14.157 6.875 19.395 15.488 24.69C15.6423 24.7839 15.8194 24.8335 16 24.8335C16.1806 24.8335 16.3577 24.7839 16.512 24.69C25.125 19.395 26 14.157 26 11.95C26 9.216 23.761 7 21 7C18.239 7 16 10 16 10C16 10 13.761 7 11 7Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        <img onclick="addToCart(${product.id
+        })" src="images/cart-icon.svg"  class="cart-icon" id="cartIcon${product.id
+        }" />
                      </div>
                      
                 </div>`;
     })
     .join("");
-    
+
+  setHeartIcons();
+  setCartIcons();
+
 }
 function getStars(rating) {
-    let stars = ``;
-    for (let i = 0; i < rating.toFixed(0); i++) {
-        stars += `<img src="images/one-star.png" />`;
-    }
-    return stars;
+  let stars = ``;
+  for (let i = 0; i < rating.toFixed(0); i++) {
+    stars += `<img src="images/one-star.png" />`;
+  }
+  return stars;
 }
 
-getProducts();
 
 function addToWishlist(productId) {
+  const heartIcon = document.getElementById(`heartIcon${productId}`);
+  heartIcon.style.fill = "red";
+  heartIcon.style.stroke = "red";
+
   const wishlistProducts =
     JSON.parse(localStorage.getItem("wishlistProducts")) || [];
 
@@ -80,6 +88,10 @@ function addToWishlist(productId) {
 }
 
 function deleteFromWishlist(deletedProductId) {
+  const heartIcon = document.getElementById(`heartIcon${deletedProductId}`);
+  heartIcon.style.fill = "none";
+  heartIcon.style.stroke = "black";
+
   const wishlistProducts =
     JSON.parse(localStorage.getItem("wishlistProducts")) || [];
   const filteredProducts = wishlistProducts.filter(
@@ -99,12 +111,17 @@ function addToCart(productId) {
       "cartProducts",
       JSON.stringify([...cartProducts, productToAdd])
     );
+    const cartIcon = document.getElementById(`cartIcon${productId}`);
+    cartIcon.setAttribute("src", "images/check-icon.svg");
   } else {
     deleteFromCart(productId);
   }
 }
 
 function deleteFromCart(deletedProductId) {
+  const cartIcon = document.getElementById(`cartIcon${deletedProductId}`);
+  cartIcon.setAttribute("src", "images/cart-icon.svg");
+
   const cartProducts = JSON.parse(localStorage.getItem("cartProducts")) || [];
   const filteredProducts = cartProducts.filter(
     (product) => product.id !== deletedProductId
@@ -137,32 +154,66 @@ function showAllProducts() {
   productCarousel.innerHTML = products
     .map((product) => {
       return `<div class="product-card">
-                    <img class ="product-card-img" src ="${
-                      product.image
-                    }" alt = "${product.title}" />
-                    <button onclick="addToCart(${
-                      product.id
-                    })" class="add-to-cart-btn" >Add To Cart</button>
+                    <img class ="product-card-img" src ="${product.image
+        }" alt = "${product.title}" />
+                    <button onclick="addToCart(${product.id
+        })" class="add-to-cart-btn" >Add To Cart</button>
                     <p class = "discount-rate">-50%</p>
                     <h3 class ="product-title">${product.title}</h3>
                     <div class="product-prices-container">
-                        <p class ="product-price-discounted">$${
-                          product.price * 0.5
-                        }</p>
+                        <p class ="product-price-discounted">$${(
+          product.price * 0.5
+        ).toFixed(2)}</p>
                         <s class ="product-price"> $${product.price}</s>
                      </div>
-                    <p>${product.rating.rate} (${product.rating.count})</p>
+                    <p>${getStars(product.rating.rate)} (${product.rating.count
+        })</p>
+
     
                     <div class="product-card-icons">
-                        <img onclick="addToWishlist(${
-                          product.id
-                        })" src="images/wishlist-icon.svg" class="wishlist-icon" />
-                        <img onclick="addToCart(${
-                          product.id
-                        })" src="images/cart-icon.svg"  class="cart-icon" />
+                        <svg onclick="addToWishlist(${product.id
+        })" width="28" height="28" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path id="heartIcon${product.id
+        }" d="M11 7C8.239 7 6 9.216 6 11.95C6 14.157 6.875 19.395 15.488 24.69C15.6423 24.7839 15.8194 24.8335 16 24.8335C16.1806 24.8335 16.3577 24.7839 16.512 24.69C25.125 19.395 26 14.157 26 11.95C26 9.216 23.761 7 21 7C18.239 7 16 10 16 10C16 10 13.761 7 11 7Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                        <img onclick="addToCart(${product.id
+        })" src="images/cart-icon.svg"  class="cart-icon" id="cartIcon${product.id
+        }" />
                      </div>
                      
                 </div>`;
     })
     .join("");
+
+  setHeartIcons();
+  setCartIcons();
+}
+
+function setHeartIcons() {
+  const wishlistProducts =
+    JSON.parse(localStorage.getItem("wishlistProducts")) || [];
+  wishlistProducts.forEach((product) => {
+    if (product.id) { // product.id tanımlı mı diye kontrol et
+      const heartIcon = document.getElementById(`heartIcon${product.id}`);
+      if (heartIcon) { // heartIcon null değil mi diye kontrol et
+        heartIcon.style.fill = "red";
+        heartIcon.style.stroke = "red";
+      }
+    }
+
+
+  });
+}
+function setCartIcons() {
+  const cartProducts = JSON.parse(localStorage.getItem("cartProducts")) || [];
+  cartProducts.forEach((product) => {
+    if (product.id) {
+      const cartIcon = document.getElementById(`cartIcon${product.id}`);
+      if (cartIcon) {
+        cartIcon.setAttribute("src", "images/check-icon.svg");
+      }
+    }
+
+
+  });
 }
